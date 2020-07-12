d94c23a6bbb4522771f5b8ae166e19007f2c1f14
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-07-11 20:35:09 -0700

    Add files via upload

---
+++ js/calculator.js
@@ -0,0 +1,9 @@
+var sum, name, num1, num2;
+
+        var name = prompt("Please enter your name");
+        var num1 = parseInt(prompt("Enter first number"));
+        var num2 = parseInt(prompt("Enter second number"));
+
+        var sum = num1 + num2;
+
+        document.write("Hi, " + name + "!" + " The sum of " + num1 + " + " + num2 + " is " + sum );
